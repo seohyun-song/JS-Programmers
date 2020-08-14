ab371d4a24c963a84502ade76674c55c4b15f9e5
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2020-08-14 16:47:14 +0900

    commit

--- level1/getCenterStrings.js
+++ level1/getCenterStrings.js
@@ -1,10 +1,11 @@
 function solution(s) {
     var len = s.length;
+
+    //parses a string and returns an integer for using the index
     var i = parseInt(len / 2);
+    
     if ((len % 2) === 0){
         return s[i-1]+s[i];
     } else {
         return s[i];
-    }
-    fhygffhfhj
 }
