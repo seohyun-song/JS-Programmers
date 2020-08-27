6f58bee3263edcd4b9b06c9257b318ba94d2f9de
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2020-08-27 12:53:57 +0900

    commit

--- level1/newReverseArray.js
+++ level1/newReverseArray.js
@@ -7,5 +7,16 @@ function solution(n) {
         return output;
     */
    //map: 기존의 배열에 특정 규칙을 적용해 새로운 배열을 만듦
-   return (n+'').split('').reverse().map(v => parseInt(v));
+   //return (n+'').split('').reverse().map(v => parseInt(v));
+
+   //숫자풀이
+   var arr = [];
+
+   do {
+       arr.push(n%10);
+       n = Math.floor(n/10);
+   } while (n>0)
+
+   return arr;
+
 }
