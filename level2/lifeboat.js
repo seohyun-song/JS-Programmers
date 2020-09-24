56d0e4fd311b1843201c49a9c4a10e026410363d
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2020-09-24 20:22:14 +0900

    greedy

--- level2/lifeboat.js
+++ level2/lifeboat.js
@@ -1,10 +1,14 @@
 function solution(people, limit) {
- 
+        /* 
+            탐욕법(greedy)
+            min + max > limit => max는 어떠한 값과도 함께 구명보트를 탈수 없다.
+        */
         var startIdx = 0;
         var endIdx = people.length-1;
         var count = 0;
         people.sort(function(a,b){return a-b});
         
+        //startIdx = endIdx => 값이 혼자 남았을 때 if문 상관없이 count++되고 while문을 나옴
         while(startIdx <= endIdx) {
             if((people[startIdx]+people[endIdx]) <= limit){
                 startIdx++;
