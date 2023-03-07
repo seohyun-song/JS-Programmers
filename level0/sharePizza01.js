8e42e763a4d2c1cf5da849b423a11f105983ace8
seohyun-song <amanda94@naver.com>
2023-03-07 11:21:08 +0900

    230307/ [add]level0 - sharePizza01~02

---
+++ level0/sharePizza01.js
@@ -0,0 +1,26 @@
+/**
+ * 머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+ */
+function solution(n) {
+  var answer = 0;
+  if (n / 7 >= 1) {
+    answer = Math.floor(n / 7);
+  }
+  if (n % 7 > 0) {
+    answer += 1;
+  }
+  return answer;
+}
+
+// 다른 사람 풀이
+function solution(n) {
+  var answer = 0;
+  n % 7 === 0 ? n / 7 : Math.floor(n / 7) + 1;
+  return answer;
+}
+
+function solution(n) {
+  var answer = 0;
+  answer = Math.ceil(n / 7); // Math.ceil(): 소수점 이하를 올림
+  return answer;
+}
