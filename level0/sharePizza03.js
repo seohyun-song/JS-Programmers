ad70c5682b080549c4b1ce07963ed20b6a4ab69f
seohyun-song <amanda94@naver.com>
2023-03-09 11:07:30 +0900

    230309/ [add] level0

---
+++ level0/sharePizza03.js
@@ -0,0 +1,16 @@
+/**
+ * 머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+ * 2 ≤ slice ≤ 10
+ * 1 ≤ n ≤ 100
+ */
+function solution(slice, n) {
+  var answer = 0;
+  answer = n % slice == 0 ? n / slice : Math.floor(n / slice) + 1;
+  return answer;
+}
+
+// 다른 사람 풀이
+// Math.ceil(): 소수점 이하를 올림
+function solution(slice, n) {
+  return Math.ceil(n / slice);
+}
